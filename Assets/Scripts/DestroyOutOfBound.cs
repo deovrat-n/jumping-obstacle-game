01ebbf467efee048d7d0aa5c02779a845413e527
deovrat-n <deovrat19101@iiitnr.edu.in>
2022-09-30 02:12:43 +0530

    first commit

---
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBound : MonoBehaviour
+{
+    public float topBound = 34.0f;
+    public float lowerBound = -11.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  //destorying the objects which are not in the screen
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("GameOver!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnPosZ = 12.0f;
+    private float spawnRangeX = 12.0f;
+    private float startDelay = 2.0f;
+    private float startInterval = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpwanRandomAnimals", startDelay, startInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    void SpwanRandomAnimals()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
